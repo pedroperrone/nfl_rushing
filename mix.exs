793bbef5567aa8ad0b5c17a8e3c10dee6d255262
Pedro Perrone <psperrone@hotmail.com>
2020-11-07 17:11:44 -0300

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule NflRushing.MixProject do
   defp deps do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ecto_sql, "~> 3.4"},
       {:ex_machina, "~> 2.4"},
       {:faker, "~> 0.16.0"},
